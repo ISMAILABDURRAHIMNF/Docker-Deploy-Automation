a751f5f38cae4459a28f3d81b4215e97b37fb8ab
Ismail Abdurrahim <isma21173ti@student.nurulfikri.ac.id>
2024-08-23 20:32:47 +0700

    MySQL connector using ENV

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import shutil
 import docker
 from flask import Flask, request, jsonify
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
 
 conn = mysql.connector.connect(
-    host='',
-    user='',
-    password='',
-    database=''
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
     )
 
 cursor = conn.cursor()  
