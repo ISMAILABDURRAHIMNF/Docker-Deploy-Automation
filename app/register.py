fc4a852522dfe8d3957d747546e50d3ffac4c343
abdurrahim <isma21173ti@student.nurulfikri.ac.id>
2025-01-21 12:21:15 +0000

    Update Upload Form

--- app/register.py
+++ app/register.py
@@ -11,13 +11,13 @@ def register_user():
     email = data.get('email')
     password = data.get('password')
 
-    check_account = query_db('SELECT username FROM akun WHERE username = %s OR email = %s', (username, email), one=True)
+    check_account = query_db('SELECT username FROM akun WHERE username = %s OR email = %s', (username, email), one=False)
 
     if check_account:
         return jsonify({'message' : 'Username atau email sudah terpakai!'}), 401
 
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    query_db('INSERT INTO akun (username, email, password) VALUES (%s,%s, %s)', (username, email, hashed_password))
+    query_db('INSERT INTO akun (username, email, password) VALUES (%s, %s, %s)', (username, email, hashed_password))
     
-    return jsonify({'message': 'Akun berhasil dibuat!'}), 201
+    return jsonify({'message': 'Akun berhasil dibuat!'}), 200
